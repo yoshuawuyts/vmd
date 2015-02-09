8daf0395599cd658bad570227c69b24ed32c833e
Yoshua Wuyts <i@yoshuawuyts.com>
2015-02-09 23:00:48 +0100

    client: remove log

--- client.js
+++ client.js
@@ -3,7 +3,6 @@ const ipc = require('ipc')
 
 ipc.on('md', function (raw) {
   const md = marked(raw)
-    console.log(raw)
   const body = document.querySelector('.markdown-body')
   body.innerHTML = md
 })
