3b0cba5d9a696b1714c20b66c0e987d0896676ad
Max Kueng <me@maxkueng.com>
2015-02-12 00:23:05 +0100

    reload content on file change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const assert = require('assert')
 const app = require('app')
 const ipc = require('ipc')
 const fs = require('fs')
+const chokidar = require('chokidar')
 
 require('crash-reporter').start()
 
@@ -10,6 +11,10 @@ const mainWindow = null
 
 const filePath = process.argv[2]
 assert(filePath, 'no file path specified')
+
+const watcher = chokidar.watch(filePath, {
+  usePolling: true
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -19,11 +24,15 @@ app.on('window-all-closed', function () {
 app.on('ready', function () {
   window = new BrowserWindow({ width: 800, height: 600 })
   window.loadUrl('file://' + __dirname + '/index.html')
-  window.webContents.on('did-finish-load', function () {
-    var file = fs.readFileSync(filePath, { encoding: 'utf8' })
-    window.webContents.send('md', file)
-  })
+  window.webContents.on('did-finish-load', sendMarkdown)
   window.on('closed', function () {
     mainWindow = null
   })
+
+  watcher.on('change', sendMarkdown)
+
+  function sendMarkdown () {
+      var file = fs.readFileSync(filePath, { encoding: 'utf8' })
+      window.webContents.send('md', file)
+  }
 })
