d64f9499394902df291522ddeadafd1dfd79821e
Max Kueng <me@maxkueng.com>
2015-02-14 13:30:04 +0100

    exit if markdown file doesn't exist

--- cli.js
+++ cli.js
@@ -3,12 +3,18 @@
 var atom = require('atom-shell')
 var proc = require('child_process')
 var path = require('path')
+var fs = require('fs')
 
 var serverPath = path.join(__dirname, 'server.js')
 
 var md = process.argv[2]
 if (!md) {
   console.error('No file path specified')
+  process.exit(1)
+}
+
+if (!fs.existsSync(path.resolve(md))) {
+  console.error('Cannot access', md + ': No such file')
   process.exit(1)
 }
 
