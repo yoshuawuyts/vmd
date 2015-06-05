cc132e04d807eb211e8c0df6c9498c26dea4291f
waynezhang <linghua.zhang@me.com>
2015-06-05 11:24:12 +0900

    menu: add
    mod. app menu add. print func
    
    fix. standard test

--- client.js
+++ client.js
@@ -20,3 +20,29 @@ ipc.on('md', function (raw) {
 window.addEventListener('keydown', function (ev) {
   if (ev.keyCode === 27) remote.getCurrentWindow().close()
 })
+
+// menu
+var template = [
+  {
+    label: 'vmd',
+    submenu: [
+      { label: 'About vmd', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Services', submenu: [] },
+      { type: 'separator' },
+      { label: 'Hide vmd', accelerator: 'Command+H', selector: 'hide:' },
+      { label: 'Hide Others', accelerator: 'Command+Shift+H', selector: 'hideOtherApplications:' },
+      { label: 'Show All', selector: 'unhideAllApplications:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click: function () { remote.require('app').quit() } }
+    ]
+  },
+  { label: 'File',
+    submenu: [
+      { label: 'Print', accelerator: 'Command+P', click: function () { window.print() } }
+    ]
+  }
+]
+
+var Menu = remote.require('menu')
+Menu.setApplicationMenu(Menu.buildFromTemplate(template))
