929ca92671a926a5ad02380cdf8863e095b6e2f0
mattdesl <dave.des@gmail.com>
2015-07-06 17:41:07 -0400

    add stdin support

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
-const proc = require('child_process')
+const spawn = require('child_process').spawn
 const electron = require('electron-prebuilt')
 const path = require('path')
 const fs = require('fs')
 
 const serverPath = path.join(__dirname, '../server.js')
-
 const md = process.argv[2]
-if (!md) {
-  console.error('No file path specified')
-  process.exit(1)
-}
+var args = [serverPath]
 
-if (!fs.existsSync(path.resolve(md))) {
-  console.error('Cannot access ', md + ': No such file')
-  process.exit(1)
+if (md) {
+  if (!fs.existsSync(path.resolve(md))) {
+    console.error('Cannot access ', md + ': No such file')
+    process.exit(1)
+  }
+  args.push(md)
 }
 
 // spawn electron
-proc.spawn(electron, [serverPath, md])
+var proc = spawn(electron, args)
+
+// pipe stdin into child process
+if (!md) {
+  process.stdin.pipe(proc.stdin)
+}
