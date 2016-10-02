1de9dbbb6eb2e596ca9fcb9ff989b694537f0b26
Max Kueng <me@maxkueng.com>
2016-10-02 15:42:37 +0200

    Switch from marked to remark and fix #90

--- renderer/render-markdown.js
+++ renderer/render-markdown.js
@@ -1,75 +1,154 @@
-const marked = require('marked')
-const highlightjs = require('highlight.js')
+const assign = require('object-assign')
+const remark = require('remark')
+const slug = require('remark-slug')
+const hljs = require('remark-highlight.js')
+const emojiToGemoji = require('remark-emoji-to-gemoji')
+const html = require('remark-html')
+const visit = require('unist-util-visit')
+const toString = require('mdast-util-to-string')
 
-const emojiRegex = /:([A-Za-z0-9_\-\+\xff]+?):/g
-const listItemRegex = /^\[(x|\s)\]\s*(.+)$/
+module.exports = renderMarkdown
 
-function escapeUnderscore (str) {
-  return str.replace(/[_]/g, '\xff')
-}
-
-function unescapeUnderscore (str) {
-  return str.replace(/[\xff]/g, '_')
-}
-
-function escapeEmoji (str) {
-  return str.replace(emojiRegex, function (match, emojiname) {
-    return ':' + escapeUnderscore(emojiname) + ':'
+const renderer = remark()
+  .use(emojiToGemoji)
+  .use(gemojiToImages)
+  .use(fixHeadings)
+  .use(fixCheckListStyles)
+  .use(slug)
+  .use([hljs, html], {
+    sanitize: false
   })
+
+function renderMarkdown (text) {
+  return renderer.process(text).toString()
 }
 
-function unescapeEmoji (str) {
-  return str.replace(emojiRegex, function (match, emojiname) {
-    return ':' + unescapeUnderscore(emojiname) + ':'
-  })
-}
+function gemojiToImages (remark) {
+  function extractTextNode (string, start, end) {
+    const startLine = string.slice(0, start).split('\n')
+    const endLine = string.slice(0, end).split('\n')
 
-var renderer = new marked.Renderer()
+    const position = {
+      start: {
+        line: startLine.length,
+        column: startLine[startLine.length - 1].length + 1
+      },
+      end: {
+        line: endLine.length,
+        column: endLine[endLine.length - 1].length + 1
+      }
+    }
 
-renderer.text = function (text) {
-  return text.replace(emojiRegex, function (match, emojiname) {
-    emojiname = unescapeUnderscore(emojiname)
-    return '<img alt=":' + emojiname + ':" src="emoji://' + emojiname + '" />'
-  })
-}
+    const textNode = {
+      type: 'text',
+      value: string.slice(start, end),
+      position: position
+    }
 
-var originalListItemRenderer = marked.Renderer.prototype.listitem
-
-renderer.listitem = function (text) {
-  var match = listItemRegex.exec(text)
-  if (match) {
-    var label = match[2]
-    var checked = match[1] === 'x' ? 'checked' : ''
-
-    text = '<label><input type="checkbox" class="task-list-item-checkbox" disabled ' + checked + ' /> ' + label + '</label>'
-
-    return '<li class="task-list-item">' + text + '</li>'
+    return textNode
   }
 
-  return originalListItemRenderer(text)
-}
+  return function transformer (tree) {
+    const reg = /:([^:]+):/g
 
-var originalInlineOutput = marked.InlineLexer.prototype.output
+    visit(tree, 'text', function (node, index, parent) {
+      // Because adding nodes to parent.children changes the indices and the
+      // index provided by `visit` is therefore wrong we need to find the new
+      // index
+      const actualIndex = parent.children.reduce(function (actualIndex, child, index) {
+        return child === node ? index : actualIndex
+      }, null)
 
-marked.InlineLexer.prototype.output = function (src) {
-  return unescapeEmoji(originalInlineOutput.call(this, escapeEmoji(src)))
-}
+      const nodes = []
+      let lastIndex = 0
+      let m
 
-var originalCodeRenderer = marked.Renderer.prototype.code
+      while ((m = reg.exec(node.value)) !== null) {
+        const gemojiLength = m[0].length
+        const gemojiName = m[1]
 
-renderer.code = function (code, language) {
-  var html = originalCodeRenderer.call(this, code, language)
-  return html.replace(/<pre>/, '<pre class="hljs">')
-}
+        if (m.index !== lastIndex) {
+          const textNode = extractTextNode(node.value, lastIndex, m.index)
+          lastIndex += textNode.value.length
+          nodes.push(textNode)
+        }
 
-marked.setOptions({
-  renderer: renderer,
-  smartLists: true,
-  highlight: function (code, lang) {
-    return highlightjs.highlightAuto(code, [lang]).value
+        const imageNode = {
+          type: 'image',
+          data: {
+            hProperties: {
+              align: 'absmiddle',
+              alt: ':' + gemojiName + ':',
+              className: 'emoji'
+            }
+          },
+          url: 'emoji://' + gemojiName,
+          title: ':' + gemojiName + ':'
+        }
+
+        nodes.push(imageNode)
+
+        lastIndex += gemojiLength
+      }
+
+      if (lastIndex !== node.value.length) {
+        const textNode = extractTextNode(node.value, lastIndex, node.value.length)
+        nodes.push(textNode)
+      }
+
+      const beforeNodes = parent.children.slice(0, actualIndex)
+      const afterNodes = parent.children.slice(actualIndex + 1)
+
+      parent.children = [].concat(
+        beforeNodes,
+        nodes,
+        afterNodes
+      )
+    })
   }
-})
+}
 
-module.exports = function (src) {
-  return marked(src)
+function fixCheckListStyles (remark) {
+  return function transformer (tree) {
+    visit(tree, 'listItem', function (node) {
+      if (node.checked !== null) {
+        const data = patchNode(node, 'data', {})
+        patchNode(data, 'hProperties', {
+          className: 'task-list-item'
+        })
+      }
+    })
+  }
+}
+
+function fixHeadings (remark) {
+  const reg = /^([#]+)\s?(.+)$/
+
+  return function transformer (tree) {
+    visit(tree, 'paragraph', function (node, index, parent) {
+      const nodeText = toString(node)
+      if (parent.type === 'root' && reg.test(nodeText)) {
+        const nodeTextParts = reg.exec(nodeText)
+        node.type = 'heading'
+        node.depth = nodeTextParts[1].length
+
+        node.children = [].concat(node.children)
+          .map(function (child, index) {
+            if (child.type === 'text' && index === 0) {
+              return assign({}, child, {
+                value: nodeTextParts[2]
+              })
+            }
+          })
+      }
+    })
+  }
+}
+
+function patchNode (context, key, value) {
+  if (!context[key]) {
+    context[key] = value
+  }
+
+  return context[key]
 }
